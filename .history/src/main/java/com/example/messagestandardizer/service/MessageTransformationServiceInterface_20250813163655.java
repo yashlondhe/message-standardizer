15187022a7a80b3a2ccb3562a43e6a3b3e0d5267
Yash Londhe <yash.londhe@rebelfoods.com>
2025-08-13 19:25:24 +0530

    Remove ignored directories

--- .history/src/main/java/com/example/messagestandardizer/service/MessageTransformationServiceInterface_20250813163655.java
+++
@@ -1,7 +0,0 @@
-package com.example.messagestandardizer.service;
-
-import com.example.messagestandardizer.model.common.StandardMessage;
-
-public interface MessageTransformationServiceInterface<T> {
-    StandardMessage transform(T message);
-}
